5264214362bcc72ca43ef3ee31b9fe9846fd2a3d
Bhavya Chaudhary <bhavyabbps14@gmail.com>
2021-07-25 17:53:26 +0530

    weekly251

---
+++ weekly251/test.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long 
+#define endl "\n" 
+
+class Solution {
+public:
+    
+    int match(vector<int> &student , vector<int> &mentor){
+        int n = student.size();
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            if(student[i] == mentor[i])
+                ans++;
+        }
+        
+        return ans;
+    }
+    
+    void compatibility(int i, vector<vector<int>>& students, vector<vector<int>>& mentors, int ans, int &maxAns, vector<bool> &vis){
+        if(i == students.size()){
+            maxAns = max(maxAns, ans);
+            return ;
+        }
+        
+        for(int j = 0; j < mentors.size(); j++){
+            if(!vis[j]){
+                vis[j] = 1;
+                ans += match(students[i], mentors[j]);
+
+                compatibility(i+1,students,mentors,ans,maxAns,vis);
+                
+                ans -= match(students[i], mentors[j]);
+                vis[j] = 0;
+            } 
+        }
+    }
+    
+    int maxCompatibilitySum(vector<vector<int>>& students, vector<vector<int>>& mentors) {
+        int m = students.size();
+        vector<bool> vis(m,0);
+        int maxAns = 0, ans = 0;
+        compatibility(0,students,mentors,0,ans,vis);
+        return ans;
+    }
+};
+
+int main(){
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+
+}
