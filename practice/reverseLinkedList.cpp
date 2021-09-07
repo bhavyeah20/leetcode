0b0529d1fad062cfc1c7a8da1705e18ee3257088
Bhavya Chaudhary <bhavyabbps14@gmail.com>
2021-09-07 19:59:46 +0530

    grinding

--- practice/reverseLinkedList.cpp
+++ practice/reverseLinkedList.cpp
@@ -23,3 +23,40 @@ public:
         return prev;
     }
 };
+
+//or
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    
+    ListNode *returnsTail(ListNode *node, ListNode **head){
+    
+        if(node->next == NULL)
+            *head = node;
+
+        else{
+            ListNode *temp = returnsTail(node->next,head);
+            temp->next = node;
+        }
+    
+        node->next = NULL;
+        return node;
+    }
+    
+    
+    ListNode* reverseList(ListNode* head) {
+        if(!head) return head;
+        ListNode *newTail = returnsTail(head,&head);
+        return head;
+    }
+};
